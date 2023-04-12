2d1f3d179ea29300347ad87761e538cff807eb79
Frants <118231497+franik7@users.noreply.github.com>
2023-04-12 19:43:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 // If all requirements are met, return true.
 //Otherwise: false
 
-
 // isValidPassword('89Fjj1nms', 'dogLuvr');  //true
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
